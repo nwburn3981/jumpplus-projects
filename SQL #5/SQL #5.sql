34e23f13e6744ab9945e92666d08ece9ff9396f1
Nicholas Burns <42648266+nwburn3981@users.noreply.github.com>
2022-09-20 13:33:24 -0400

    Almost done

--- SQL #5/SQL #5.sql
+++ SQL #5/SQL #5.sql
@@ -62,7 +62,7 @@ Insert into StudentDetails
 Values(
 41,
 'Kit Beget',
-1115487,
+null,
 '2020-06-17');
 
 #Query 2
@@ -93,6 +93,126 @@ ON StudentDetails.studId = StudentStipend.stuId
 WHERE Stipend >= 50000 && Stipend <= 100000;
 
 #Query 7
+SELECT stuName, stipend
+FROM StudentDetails
+LEFT JOIN
+StudentStipend
+ON StudentDetails.studId = StudentStipend.stuId;
+
+#Query 8
+SELECT *
+FROM StudentDetails
+RIGHT JOIN
+StudentStipend
+ON StudentDetails.studId = StudentStipend.stuId;
+
+#Query 9
+#Write an SQL query to fetch the number of students working in project ‘P1’
+SELECT COUNT(StuId), Project
+FROM StudentStipend
+WHERE Project ='P1'; 
+
+#Query 10. Write an SQL query for fetching duplicate records from a table.
+SELECT *
+FROM StudentDetails
+GROUP BY StuName
+HAVING COUNT(StuName) > 1;
+
+
+#Query 11. Write an SQL query for removing duplicates from a table without using a temporary table.
+DELETE
+FROM StudentDetails
+WHERE StudId NOT IN(
+SELECT * FROM (
+SELECT MAX(StudId)
+FROM StudentDetails
+GROUP BY stuName
+) as max
+) 
+;
+
+#Query 12. Write an SQL query for fetching all the Students who also have enrollment No from StudentDetails table.
+SELECT *
+FROM StudentDetails
+WHERE EnrollmentNo IS NOT NULL;
+
+#Query 13. Write an SQL query for creating a new table with data and structure copied from another table.
+CREATE TABLE student_backup 
+AS
+SELECT *
+FROM StudentDetails;
+
+#Query 14. Write an SQL query to fetch a joint record between two tables using intersect.
+SELECT DISTINCT * FROM StudentDetails
+INNER JOIN
+StudentStipend ON StudentDetails.studId = StudentStipend.stuId;
+
+#Query 15. Write an SQL query for fetching records that are present in one table but not in another table using Minus.
+
+
+#Query 16. Write an SQL query to fetch count of students project-wise sorted by project’s count in descending order.
+SELECT  stuId, Project, Stipend, Count(Project) as project_members
+FROM StudentStipend
+GROUP BY Project
+ORDER BY project_members DESC;
+
+#Query 17. Write an SQL query for creating an empty table with the same structure as some other table.
+CREATE TABLE student_backup_backup
+LIKE StudentDetails;
+
+#Query 18. Write an SQL query for finding current date-time.
+SELECT current_timestamp();
+
+#Query 19. Write an SQL query for fetching only even rows from the table.
+
+SELECT *
+FROM ( 
+SELECT row_number() 
+OVER (order by studId)AS line , studId, stuName, EnrollmentNo, DateOfJoining
+FROM StudentDetails) as orderTable
+WHERE Mod(line, 2) = 0;
+
+#Query 20. Write an SQL query for fetching all the Students details from StudentDetails table who joined in the Year 2018.
+SELECT *
+FROM StudentDetails
+WHERE DateofJoining > '2018-12-12';
+
+#Query 21. Write the SQL query to find the nth highest stipend from the table.
+SELECT *
+FROM ( 
+SELECT row_number() 
+OVER (order by Stipend DESC)AS line , stuId, Project, Stipend
+FROM StudentStipend) as orderedTable
+WHERE line = 3;
+
+#Query 22. Write SQL query for fetching top n records using LIMIT?
+SELECT *
+FROM StudentStipend
+Order BY Stipend DESC
+LIMIT 2;
+
+#Query 23. Write a query for fetching only the first name(string before space) from the Name column of StudentDetails table.
+SELECT substring_index( stuName, ' ', 1)
+FROM StudentDetails;
+
+#Query 24. Write an SQL query for fetching only odd rows from the table.
+
+SELECT *
+FROM ( 
+SELECT row_number() 
+OVER (order by studId)AS line , studId, stuName, EnrollmentNo, DateOfJoining
+FROM StudentDetails) as orderTable
+WHERE Mod(line, 2) = 1;
+
+#Query 25. Write SQL query for finding the 3rd highest stipend from the table without using TOP/limit keyword.
+SELECT *
+FROM ( 
+SELECT row_number() 
+OVER (order by Stipend DESC)AS line , stuId, Project, Stipend
+FROM StudentStipend) as orderedTable
+WHERE line = 3;
+
+#Query Bonus : Try to create at least 3 bonus queries which contain 3 subqueries , can use Sakila database for that
 
 
 
